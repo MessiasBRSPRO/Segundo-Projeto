4e85c875f1032ea1518ab6b0a2f7af281ded9d04
First Commit <meddisme@gmail.com>
2024-04-17 16:21:53 -0300

    Equals and hashcode in User Class, emails equals represents a same user

--- src/User.java
+++ src/User.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class User {
     private String mail;
     /*The encapsulation is one of main pillars of OOP, the objective is give access's restriction
@@ -8,6 +10,19 @@ public class User {
     public User(String mail, String password){
         this.mail = MailValidator.mailValidatorMethod(mail);
         this.password = PasswordValidator.passwordValidator(password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(mail, user.mail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mail);
     }
 
     public String getMail() {
