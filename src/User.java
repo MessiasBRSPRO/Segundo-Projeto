44a636dbb8bdbf00da769c43e74c72e321742de2
First Commit <meddisme@gmail.com>
2024-04-18 19:43:58 -0300

    Adding an Data's Structure/Collections in User class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class User {
@@ -6,6 +9,12 @@ public class User {
     to data into the attributes avoiding changes or access the value of attributes.
     * */
     private String password;
+
+    private static List<User> userList = new ArrayList<>();
+    /*this is a Collection. The arrayList
+    * is a linear data's Structure than not is synchronized, and different of
+    * vector, the size of array is dynamic.The data are saved in array sequentially
+    * and each data have a index(starts in index 0) */
 
     public User(String mail, String password){
         this.mail = MailValidator.mailValidatorMethod(mail);
@@ -23,6 +32,20 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(mail);
+    }
+
+    public static void registerUserInArrayList(User user){
+        if(!(userList.contains(user))){
+            userList.add(user);
+            System.out.println("User:"+user.getMail() + " hass been registered.");
+        }else{
+            System.out.println("The mail:"+user.getMail());
+            System.out.println("This email/user already exists!");
+        }
+    }
+
+    public static List<User> getUserList() {
+        return Collections.unmodifiableList(userList);
     }
 
     public String getMail() {
