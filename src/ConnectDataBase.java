90f6be1b9de2e6453409f059694dc86deacb54ab
First Commit <meddisme@gmail.com>
2024-04-19 19:04:06 -0300

    Created the class than integrate a DataBase

---
+++ src/ConnectDataBase.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDataBase {
+    private Connection connection;
+    private String urlConnection;
+    private String userDataBase = "postgres";
+    private String passwordDataBase = "Afton3444$";
+
+    public Connection startConnection(String dataBaseName, int port, String tableName){
+        urlConnection = "jdbc:"+dataBaseName + "://localhost:"+port +"/"+tableName;
+        try {
+            connection = DriverManager.getConnection(urlConnection, userDataBase, passwordDataBase);
+            if(connection != null){
+                System.out.println(dataBaseName + " connected in "+ tableName);
+                return connection;
+            }
+        }catch (SQLException e){
+            System.out.println("an Exception has occurred:"+e.getMessage());
+            System.out.println("Occurred in:"+e.getStackTrace());
+        }
+        return connection;
+    }
+}
