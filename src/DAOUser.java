7f21490f9385a987e8bf2d94f1c1bbc07b315454
First Commit <meddisme@gmail.com>
2024-04-19 19:46:13 -0300

    Created class Data Access Object of User class/DB

---
+++ src/DAOUser.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DAOUser {
+
+    private ConnectDataBase connectDataBase;
+    private Connection connection;
+    private String sqlCommand;
+
+    public DAOUser(){
+        this.connectDataBase = new ConnectDataBase();
+        this.connection = connectDataBase.startConnection("postgresql", 5432, "usuarios");
+    }
+    public void insertIntoDataBase(User user){
+        sqlCommand = "INSERT INTO usuarios (mail, password) VALUES(?, ?)";
+        try{
+            PreparedStatement operationsSQLExecutor = connection.prepareStatement(sqlCommand);
+            operationsSQLExecutor.setString(1, user.getMail());
+            operationsSQLExecutor.setString(2, user.getPassword());
+            System.out.println(user.getMail() + " has registered in dataBase");
+            operationsSQLExecutor.execute();
+        }catch (SQLException e){
+            System.out.println("an exception has occurred:"+e.getMessage());
+        }
+    }
+}
