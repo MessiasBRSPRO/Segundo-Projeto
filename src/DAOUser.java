4a4361990d081a8d6395cda21441eb6704ee74b6
First Commit <meddisme@gmail.com>
2024-04-20 09:36:58 -0300

    Add the Select all rows of DB in DAOUser

--- src/DAOUser.java
+++ src/DAOUser.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DAOUser {
@@ -26,12 +27,27 @@ public class DAOUser {
     }
 
     public void deleteFromDataBase(User user){
-        sqlCommand = "DELETE FROM usuarios WHERE mail=? AND password=?";
+        sqlCommand = "DELETE FROM usuarios WHERE mail=?";
         try{
             PreparedStatement operationsSQLExecutor = connection.prepareStatement(sqlCommand);
             operationsSQLExecutor.setString(1, user.getMail());
-            operationsSQLExecutor.setString(2, user.getPassword());
             operationsSQLExecutor.execute();
+        }catch (SQLException e){
+            System.out.println("an exception has occurred:"+e.getMessage());
+        }
+    }
+
+    public void seeAllRows(){
+        sqlCommand = "SELECT * FROM usuarios";
+        try{
+            PreparedStatement operationsSQLExecutor = connection.prepareStatement(sqlCommand);
+            ResultSet rows = operationsSQLExecutor.executeQuery();
+            int totalRows = 0;
+            while(rows.next()){
+                totalRows++;
+                System.out.println("User Mail:"+rows.getString(1) + " | password:"+rows.getString(2));
+            }
+            System.out.println("Total rows:"+totalRows);
         }catch (SQLException e){
             System.out.println("an exception has occurred:"+e.getMessage());
         }
