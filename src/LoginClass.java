3ecd0baff59fc9b94feb748b78fa21d8b9af9333
First Commit <meddisme@gmail.com>
2024-05-03 16:43:31 -0300

    Creating the logic of send of Mail and forgotten password

--- src/LoginClass.java
+++ src/LoginClass.java
@@ -5,6 +5,7 @@ public class LoginClass {
     private DAOUser daoUser;
     private boolean isLogged;
     private EmailService emailService;
+    private final Scanner inputMain = new Scanner(System.in);
 
     public LoginClass(){
         this.daoUser = new DAOUser();
@@ -13,10 +14,7 @@ public class LoginClass {
     }
 
     public void startLogin(){
-        Scanner inputMain = new Scanner(System.in);
-
         System.out.println("= = = = = = LOGIN = = = = = =");
-
         while(!isLogged){
 
             System.out.println("Email:");
@@ -33,6 +31,25 @@ public class LoginClass {
 
             if(!(resultsRow)){
                 System.out.println("User email or password invalid or don't exists");
+                System.out.println("Your has forgotten the password?[1]yes[2]no");
+                int optionForgotten = inputMain.nextInt();
+
+                switch (optionForgotten){
+                    case 1:
+                        this.forgetMyPassword(user);
+                        break;
+                    case 2:
+                        System.out.println("try login again");
+                        break;
+                    default:
+                        try{
+                            throw new InvalidOptionException("Invalid option, choose 1 or 2!");
+                        }catch (InvalidOptionException e){
+                            System.out.println("an exception has occurred:"+e.getMessage());
+                        }
+                        break;
+
+                }
             }else {
                 System.out.println("Access allowed");
                 isLogged = true;
@@ -40,4 +57,36 @@ public class LoginClass {
         }
 
     }
+    private void forgetMyPassword(User user){
+        emailService.sendVerificationCode(user.getMail());
+        int codeInvalid = 0;
+        boolean changed = false;
+        System.out.println("An Verification code has sent to your Register's Mail");
+        while(!changed){
+            System.out.println("Insert the 6 digits Code here:");
+            int codeInput = inputMain.nextInt();
+
+            try{
+                if(!(codeInput == emailService.getCodeVerification())){
+                    codeInvalid++;
+                    System.out.println("Try again");
+                    if(codeInvalid == 5){
+                        System.out.println("Exceeded tentatives, please try again later! ");
+                    }
+                    throw new VerificationCodeException("code verification is incorrect");
+                }else{
+                    System.out.println("Insert the new password:");
+                    String newPassword = inputMain.next();
+                    user.setPassword(newPassword);
+                    daoUser.updatePassword(newPassword, user);
+                    changed = true;
+                    isLogged = true;
+                }
+            }catch (VerificationCodeException e){
+                System.out.println("an exception has occurred:"+e.getMessage());
+            }
+        }
+    }
+
+    private void forgetMyMail(User user){}
 }
