2a5bf55f4ab18424214540635f43489962846e57
First Commit <meddisme@gmail.com>
2024-05-03 21:27:21 -0300

    add comments in LoginClass

--- src/LoginClass.java
+++ src/LoginClass.java
@@ -7,15 +7,15 @@ public class LoginClass {
     private EmailService emailService;
     private final Scanner inputMain = new Scanner(System.in);
 
-    public LoginClass(){
+    public LoginClass() {
         this.daoUser = new DAOUser();
         this.isLogged = false;
         this.emailService = new EmailService();
     }
 
-    public void startLogin(){
+    public void startLogin() {
         System.out.println("= = = = = = LOGIN = = = = = =");
-        while(!isLogged){
+        while (!isLogged) {
 
             System.out.println("Email:");
             String inputMail = inputMain.next();
@@ -25,16 +25,15 @@ public class LoginClass {
 
             final User user = new User("", inputMail, inputPassword);
 
-            //Sarching the user in DataBase postgreSQL
             daoUser.specifiedRowSearch(user);
             boolean resultsRow = DAOUser.getTotalRows() == 1;
 
-            if(!(resultsRow)){
+            if (!(resultsRow)) {
                 System.out.println("User email or password invalid or don't exists");
                 System.out.println("Your has forgotten the password?[1]yes[2]no");
                 int optionForgotten = inputMain.nextInt();
 
-                switch (optionForgotten){
+                switch (optionForgotten) {
                     case 1:
                         this.forgetMyPassword(user);
                         break;
@@ -42,39 +41,41 @@ public class LoginClass {
                         System.out.println("try login again");
                         break;
                     default:
-                        try{
+                        try {
                             throw new InvalidOptionException("Invalid option, choose 1 or 2!");
-                        }catch (InvalidOptionException e){
-                            System.out.println("an exception has occurred:"+e.getMessage());
+                        } catch (InvalidOptionException e) {
+                            System.out.println("an exception has occurred:" + e.getMessage());
                         }
                         break;
 
                 }
-            }else {
+            } else {
                 System.out.println("Access allowed");
                 isLogged = true;
             }
         }
 
     }
-    private void forgetMyPassword(User user){
+
+    //This method is responsible of modify the password of an user utilizing an Email service
+    private void forgetMyPassword(User user) {
         emailService.sendVerificationCode(user.getMail());
         int codeInvalid = 0;
         boolean changed = false;
         System.out.println("An Verification code has sent to your Register's Mail");
-        while(!changed){
+        while (!changed) {
             System.out.println("Insert the 6 digits Code here:");
             int codeInput = inputMain.nextInt();
 
-            try{
-                if(!(codeInput == emailService.getCodeVerification())){
+            try {
+                if (!(codeInput == emailService.getCodeVerification())) {
                     codeInvalid++;
                     System.out.println("Try again");
-                    if(codeInvalid == 5){
+                    if (codeInvalid == 5) {
                         System.out.println("Exceeded tentatives, please try again later! ");
                     }
                     throw new VerificationCodeException("code verification is incorrect");
-                }else{
+                } else {
                     System.out.println("Insert the new password:");
                     String newPassword = inputMain.next();
                     user.setPassword(newPassword);
@@ -82,11 +83,9 @@ public class LoginClass {
                     changed = true;
                     isLogged = true;
                 }
-            }catch (VerificationCodeException e){
-                System.out.println("an exception has occurred:"+e.getMessage());
+            } catch (VerificationCodeException e) {
+                System.out.println("an exception has occurred:" + e.getMessage());
             }
         }
     }
-
-    private void forgetMyMail(User user){}
 }
